5ea3898e3b0f8f3e5f43655779a5fbbfb53c95f1
Vladimir Fibe <motiw@icloud.com>
2023-09-02 12:16:34 +0600

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Xcode on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
